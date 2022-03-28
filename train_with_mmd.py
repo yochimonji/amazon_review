a4c9a08f2d98a872904235c03865dadf8235803a
yochimonji <yochimonji@gmail.com>
2022-03-28 08:05:54 +0000

    add MMD

---
+++ train_with_mmd.py
@@ -0,0 +1,158 @@
+import random
+
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from torchtext.legacy import data
+from torchtext.vocab import Vectors
+from tqdm import tqdm
+
+from util import calc_accuracy, calc_f1, init_device, load_params
+from util.mmd import compute_mmd, run_test
+from util.model import MyEmbed, MyMlp
+from util.nlp_preprocessing import dataframe2dataset, tokenizer
+
+
+def train():
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    print("Loading parameters...")
+    params = load_params("config/params_mmd.json")
+
+    device = init_device()
+
+    # データセット読み込み
+    train_df = pd.read_json(params["train_data_path"], orient="record", lines=True)
+    if params["is_developing"]:
+        train_df = train_df.sample(n=5000, random_state=1)
+    dev_df = pd.read_json(params["dev_data_path"], orient="record", lines=True)
+    test_df = pd.read_json(params["test_data_path"], orient="record", lines=True)
+
+    train_source_df = train_df[train_df["product_category"] == params["source_category"]]
+    dev_source_df = dev_df[dev_df["product_category"] == params["source_category"]]
+    test_source_df = test_df[test_df["product_category"] == params["source_category"]]
+    train_target_df = train_df[train_df["product_category"] == params["target_category"]]
+    dev_target_df = dev_df[dev_df["product_category"] == params["target_category"]]
+    test_target_df = test_df[test_df["product_category"] == params["target_category"]]
+
+    if train_source_df.shape[0] < train_target_df.shape[0]:
+        print("Target more than source")
+        print(f"Source num: {train_source_df.shape[0]}, Target num: {train_target_df.shape[0]}")
+        exit()
+    train_target_num = int(min(train_source_df.shape[0] * params["target_ratio"], train_target_df.shape[0]))
+    train_target_df = train_target_df.sample(train_target_num, replace=False)
+    print(f"Source num: {train_source_df.shape[0]}, Target num: {train_target_df.shape[0]}")
+
+    print("Building data iterator...")
+    text_field = data.Field(
+        sequential=True,
+        tokenize=tokenizer,
+        use_vocab=True,
+        lower=True,
+        include_lengths=True,
+        batch_first=True,
+        fix_length=params["token_max_length"],
+        init_token="<cls>",
+        eos_token="<eos>",
+    )
+    label_field = data.Field(sequential=False, use_vocab=False)
+    fields = [("text", text_field), ("label", label_field)]
+
+    columns = ["review_body", "stars"]
+    train_source_dataset = dataframe2dataset(train_source_df, fields, columns)
+    dev_source_dataset = dataframe2dataset(dev_source_df, fields, columns)
+    test_source_dataset = dataframe2dataset(test_source_df, fields, columns)
+    train_target_dataset = dataframe2dataset(train_target_df, fields, columns)
+    dev_target_dataset = dataframe2dataset(dev_target_df, fields, columns)
+    test_target_dataset = dataframe2dataset(test_target_df, fields, columns)
+
+    japanese_fasttext_vectors = Vectors(name=params["vector_path"])
+    text_field.build_vocab(train_source_dataset, vectors=japanese_fasttext_vectors, min_freq=1)
+
+    # train_source_iterのみエポックごとに生成し直す
+    dev_source_iter = data.BucketIterator(
+        dataset=dev_source_dataset, batch_size=params["batch_size"], train=False, sort=False
+    )
+    test_source_iter = data.BucketIterator(
+        dataset=test_source_dataset, batch_size=params["batch_size"], train=False, sort=False
+    )
+    train_target_iter = data.BucketIterator(dataset=train_target_dataset, batch_size=params["batch_size"], train=True)
+    dev_target_iter = data.BucketIterator(
+        dataset=dev_target_dataset, batch_size=params["batch_size"], train=False, sort=False
+    )
+    test_target_iter = data.BucketIterator(
+        dataset=test_target_dataset, batch_size=params["batch_size"], train=False, sort=False
+    )
+
+    v_size = len(text_field.vocab.stoi)
+    embed = MyEmbed(params["emb_dim"], v_size, text_field).to(device)
+    classifier = MyMlp(params["emb_dim"], params["token_max_length"], params["class_num"]).to(device)
+    criterion = getattr(nn, params["criterion"])()
+    optimizer_embed = getattr(torch.optim, params["optimizer"])(embed.parameters(), lr=params["lr"])
+    optimizer_classifier = getattr(torch.optim, params["optimizer"])(classifier.parameters(), lr=params["lr"])
+    scheduler_embed = torch.optim.lr_scheduler.ExponentialLR(optimizer_embed, gamma=0.9)
+    scheduler_classifier = torch.optim.lr_scheduler.ExponentialLR(optimizer_classifier, gamma=0.9)
+
+    split_ratio = len(train_target_dataset) / len(train_source_dataset)
+
+    for epoch in range(params["epochs"]):
+        print(f"\nepoch {epoch+1} / {params['epochs']}")
+        total_loss = 0
+
+        random.seed(epoch)
+        train_source_subset, _ = train_source_dataset.split(split_ratio=split_ratio)
+        train_source_iter = data.BucketIterator(
+            dataset=train_source_subset, batch_size=params["batch_size"], train=True
+        )
+
+        for i, (source_batch, target_batch) in tqdm(
+            enumerate(zip(train_source_iter, train_target_iter)), total=len(train_source_iter)
+        ):
+            embed.train()
+            classifier.train()
+            optimizer_embed.zero_grad()
+            source_x, source_y = source_batch.text[0].to(device), (source_batch.label - 1).to(device)
+            target_x, target_y = target_batch.text[0].to(device), (target_batch.label - 1).to(device)
+
+            source_embed = embed(source_x)
+            source_pred = classifier(source_embed)
+            source_loss = criterion(source_pred, source_y)
+            source_loss.backward()
+
+            target_embed = embed(target_x)
+            target_pred = classifier(target_embed)
+            target_loss = criterion(target_pred, target_y)
+            target_loss.backward()
+
+            all_loss = source_loss + target_loss
+            optimizer_embed.step()
+            optimizer_classifier.step()
+            total_loss += all_loss.cpu()
+
+        scheduler_embed.step()
+        scheduler_classifier.step()
+        print(f"Train Loss: {total_loss / (len(train_source_iter) + len(train_target_iter)):.3f}")
+
+        dev_source_accuracy, dev_source_f1 = run_test(embed, classifier, dev_source_iter, device)
+        print(f"\nDev source Accuracy: {dev_source_accuracy:.2f}")
+        print(f"Dev source F1 Score: {dev_source_f1:.2f}")
+        dev_target_accuracy, dev_target_f1 = run_test(embed, classifier, dev_target_iter, device)
+        print(f"\nDev target Accuracy: {dev_target_accuracy:.2f}")
+        print(f"Dev target F1 Score: {dev_target_f1:.2f}")
+
+    test_source_accuracy, test_source_f1 = run_test(embed, classifier, test_source_iter, device)
+    print(f"\nTest source Accuracy: {test_source_accuracy:.2f}")
+    print(f"Test source F1 Score: {test_source_f1:.2f}")
+    test_target_accuracy, test_target_f1 = run_test(embed, classifier, test_target_iter, device)
+    print(f"\nTest target Accuracy: {test_target_accuracy:.2f}")
+    print(f"Test target F1 Score: {test_target_f1:.2f}")
+
+
+if __name__ == "__main__":
+    train()
---
+++ util/mmd.py
@@ -0,0 +1,45 @@
+import torch
+from torchtext.legacy import data
+
+from util import calc_accuracy, calc_f1
+
+
+def compute_kernel(x, y):
+    x_size = x.size(0)
+    y_size = y.size(0)
+    dim = x.size(1)
+    x = x.unsqueeze(1)  # (x_size, 1, dim)
+    y = y.unsqueeze(0)  # (1, y_size, dim)
+    tiled_x = x.expand(x_size, y_size, dim)
+    tiled_y = y.expand(x_size, y_size, dim)
+    kernel_input = (tiled_x - tiled_y).pow(2).mean(2) / float(dim)
+    return torch.exp(-kernel_input)  # (x_size, y_size)
+
+
+def compute_mmd(x, y):
+    x_kernel = compute_kernel(x, x)
+    y_kernel = compute_kernel(y, y)
+    xy_kernel = compute_kernel(x, y)
+    mmd = x_kernel.mean() + y_kernel.mean() - 2 * xy_kernel.mean()
+    return mmd
+
+
+def run_test(embed: torch.nn.Module, classifier: torch.nn.Module, iter: data.BucketIterator, device: torch.device):
+    total_accuracy = 0
+    total_f1 = 0
+    embed.eval()
+    classifier.eval()
+    for batch in iter:
+        x, y = batch.text[0].to(device), (batch.label - 1).to(device)
+        with torch.no_grad():
+            embed_sentence = embed(x)
+            pred = classifier(embed_sentence)
+
+        label_array = y.cpu().numpy()
+        logit_array = pred.cpu().numpy()
+
+        total_accuracy += calc_accuracy(label_array, logit_array)
+        total_f1 += calc_f1(label_array, logit_array)
+        avg_accuracy = total_accuracy / len(iter)
+        avg_f1 = total_f1 / len(iter)
+    return avg_accuracy, avg_f1
