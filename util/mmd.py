be8b33bcff178148a7acaf9a8536ad4070cd75c1
yochimonji <yochimonji@gmail.com>
2022-04-04 06:37:32 +0000

    rm 使っていないMMDの計算を削除

--- util/mmd.py
+++ util/mmd.py
@@ -4,26 +4,6 @@ from torchtext.legacy import data
 from util import calc_accuracy, calc_f1
 
 
-def compute_kernel(x, y):
-    x_size = x.size(0)
-    y_size = y.size(0)
-    dim = x.size(1)
-    x = x.unsqueeze(1)  # (x_size, 1, dim)
-    y = y.unsqueeze(0)  # (1, y_size, dim)
-    tiled_x = x.expand(x_size, y_size, dim)
-    tiled_y = y.expand(x_size, y_size, dim)
-    kernel_input = (tiled_x - tiled_y).pow(2).mean(2) / float(dim)
-    return torch.exp(-kernel_input)  # (x_size, y_size)
-
-
-def compute_mmd(x, y):
-    x_kernel = compute_kernel(x, x)
-    y_kernel = compute_kernel(y, y)
-    xy_kernel = compute_kernel(x, y)
-    mmd = x_kernel.mean() + y_kernel.mean() - 2 * xy_kernel.mean()
-    return mmd
-
-
 def run_test(model: torch.nn.Module, iter: data.BucketIterator, device: torch.device):
     total_accuracy = 0
     total_f1 = 0
